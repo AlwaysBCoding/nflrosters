a6bd4591cbfced5484aee6e9c206c60710bafb79
Jordan Leigh <AlwaysBCoding@Jordans-MacBook-Air.local>
2012-10-17 13:04:47 -0500

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+N1::Application.config.secret_token = '2fc88984a09787fbd575d2946e88528de6abdabe2a31ea5b68baa46c5bd476e63e8fecb655acbc650231cc38e0d9e778f68bdf5c8abfc14fe17a73ff007c6fb3'
---
+++ lib/tasks/populate.rake
@@ -0,0 +1,6 @@
+namespace :db do
+  desc "Erase and populate the database with fake data"
+  task :populate => :environment do
+    
+  end
+end
